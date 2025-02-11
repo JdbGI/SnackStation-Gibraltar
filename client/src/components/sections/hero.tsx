31e9c2b89c2993a2c09cea9f4aeb7f35ec0efc3e
ccdrgib <39569880-ccdrgib@users.noreply.replit.com>
2025-02-11 16:22:46 +0000

    Agent query: I've adjusted the hero image size to be larger (400px height, 380px max-width). Does the vending machine image now look properly sized and proportional to the rest of the content?
    Increase hero section image size
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/0883bebc-2891-4fea-9288-ca89697b9530/1485070d-889c-467b-9997-de3d747d8f1d.jpg

--- client/src/components/sections/hero.tsx
+++ client/src/components/sections/hero.tsx
@@ -37,7 +37,7 @@ export default function Hero() {
           <img 
             src="https://www.barton.gi/wp-content/uploads/2025/02/vhm_1739290463.png" 
             alt="SnackStation Vending Machine" 
-            className="w-auto h-[300px] max-w-[280px] object-contain"
+            className="w-auto h-[400px] max-w-[380px] object-contain"
           />
         </motion.div>
       </div>
