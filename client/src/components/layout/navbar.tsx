8b8d4ec5593bcb3104e5a3882a78a17646e39159
ccdrgib <39569880-ccdrgib@users.noreply.replit.com>
2025-02-11 11:54:40 +0000

    Agent query: Can you see the pink SnackStation logo in the navigation bar at the top of the page now? And does the pink color theme (#ff80bf) look consistent throughout the website?
    Update navbar logo to use externally hosted image.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/0883bebc-2891-4fea-9288-ca89697b9530/0e84eec3-29e2-4294-add4-372c1666015c.jpg

--- client/src/components/layout/navbar.tsx
+++ client/src/components/layout/navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
         <div className="flex h-16 items-center justify-between">
           <a href="#" className="flex items-center">
             <img 
-              src="/logo.png" 
+              src="http://www.barton.gi/wp-content/uploads/2025/02/Asset-2@3xPink.png" 
               alt="SnackStation" 
               className="h-8 w-auto"
             />
